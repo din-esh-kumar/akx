44108affd39310f3f7803fbec0eb68c12bf701f2
din-esh-kumar <dkdinesh70707@gmail.com>
2024-10-26 11:06:18 +0530

    new commit

--- akx.js
+++ akx.js
@@ -1,3 +1,18 @@
-function akinsta() {
-    document.querySelector("cards")
-}
+// document.querySelector(".mainvid").addEventListener("canplay",function(){
+//     setTimeout(function(){
+//             document.querySelector(".popup").display="block";
+//         },1000
+//     )
+// });
+// document.querySelector(".newvid").addEventListener("canplay",function() {
+//     setTimeout(function xplay(){
+//         console.log('l');
+//         document.querySelector(".popup").display="block";
+//     },
+//     1000
+//     )
+// })
+
+document.querySelector("#closed").addEventListener("click", function(){
+    document.querySelector(".popup").style.display="none";
+});
