6e90ab85d37e8850385c76373434409651a7c0a2
din-esh-kumar <dkdinesh70707@gmail.com>
2024-11-19 21:00:40 +0530

    hello

--- akx.js
+++ akx.js
@@ -12,7 +12,12 @@
 //     1000
 //     )
 // })
-
+// document.querySelector(".clickpop").function clickpop(){
+        setTimeout(function(){
+            document.querySelector(".popup").style.display="block";
+        },2000
+        )
+// });
 document.querySelector("#closed").addEventListener("click", function(){
     document.querySelector(".popup").style.display="none";
 });
