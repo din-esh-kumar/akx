e0753c725e07f214db8f33d6b12aed6aedbdaf1b
din-esh-kumar <dkdinesh70707@gmail.com>
2024-11-20 20:44:14 +0530

    nothing

--- akx.js
+++ akx.js
@@ -16,6 +16,7 @@
         setTimeout(function(){
             document.querySelector(".popup").style.display="block";
             document.querySelector(".akvid").style.visibility="hidden";
+            // document.querySelector(".wholediv").style.visibility="hidden";
             // document.querySelector(".pagebtn").style.visibility="hidden";
             // document.querySelector("body").style.opacity='0.2'
             // document.querySelector(".popup").style.opacity="1";
@@ -25,6 +26,7 @@
 document.querySelector("#closed").addEventListener("click", function(){
     document.querySelector(".popup").style.display="none";
     document.querySelector(".akvid").style.visibility="visible";
+    // document.querySelector(".wholediv").style.visibility="visible";
     // document.querySelector(".pagebtn").style.visibility="visible";
     // document.querySelector("body").style.opacity='1';
     // document.querySelector("footer").style.opacity="0";
